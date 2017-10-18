68f135c51a5a72a44cab8b57e7d2345df16c3fee
felicia-uri <felicia.uri@gmail.com>
2017-10-18 13:31:40 -0600

    Updated AtomicReduction

--- src/edu/cnm/deepdive/concurrent/AtomicReduction.java
+++ src/edu/cnm/deepdive/concurrent/AtomicReduction.java
@@ -12,30 +12,38 @@ public class AtomicReduction {
   private static final String START_MESSAGE = "Starting %d threads.%n";
   private static final String THREAD_COMPLETE_MESSAGE = "Thread %d complete.%n";
   private static final String FINISH_MESSAGE = "Sum = %, d.%n";
+  private static final String TIMING_MESSAGE = "Total time = %.3f seconds.%n";
 
   private AtomicLong sum;
+  private AtomicLong updates;
   private CountDownLatch signal;
 
   public static void main(String[] args) {
+    long startTime = System.currentTimeMillis();
     AtomicReduction race = new AtomicReduction();
     race.start();
     race.finish();
+    long finishTime = System.currentTimeMillis();
+    System.out.printf(TIMING_MESSAGE, (finishTime - startTime) / 1000.0);
   }
-
+  
   private void start() {
     System.out.printf(START_MESSAGE, NUM_THREADS);
-    signal = new CountDownLatch(NUM_THREADS);
     sum = new AtomicLong(0);
+    updates = new AtomicLong(0);
+    signal = new CountDownLatch(NUM_THREADS);
     for (int i = 0; i < NUM_THREADS; i++) {
-      new Thread (new Runnable()) {
-        
-        public void run() {
-          for (int j = 0; j < UPPER_LIMIT; j++) {
-            localSum += ThreadLocalRandom.current().nextInt(2);
+      new Thread(() -> {
+        long localSum = 0;
+        long localUpdates = 0;
+        for (int j = 0; j < UPPER_LIMIT; j++) {
+          localSum += ThreadLocalRandom.current().nextInt(2);
+          localUpdates++;
         }
-        }
+        sum.addAndGet(localSum);
+        updates.addAndGet(localUpdates);
+        System.out.printf(THREAD_COMPLETE_MESSAGE, Thread.currentThread().getId());
         signal.countDown();
-        System.out.printf(THREAD_COMPLETE_MESSAGE, Thread.currentThread().getId())
       }).start();
     }
   }
@@ -46,8 +54,8 @@ public class AtomicReduction {
     } catch (InterruptedException ex) {
       // Do nothing
     } finally {
-      System.out.printf(FINISH_MESSAGE, sum.longValue());
+      System.out.printf(FINISH_MESSAGE, updates.longValue(), sum.longValue());
     }
   }
-
+  
 }
